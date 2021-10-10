c4bcf2603df598d594d0c5e21c3e2f7dded29b3c
hershyheilpern <hershyheilpern@gmail.com>
2021-10-10 16:20:25 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const url = require('url');
+
+class obj{
+    constructor(obj){
+        for(i of Object.keys(obj)){
+            this[i] = obj[i]
+        }
+        this.domain= this.req.headers.host
+        this.url= decodeURIComponent(this.req.url.split("?")[0])
+        this.baseurl= decodeURIComponent(this.req.url).split(/\/|\?/g)[1]
+        this.qry=url.parse(this.req.url,true).query
+        this.surl = decodeURIComponent(this.req.url).split("?")[0].split(this.baseurl)[1]
+    }
+    send403(){
+        this.res.writeHead(403)
+        this.res.end()
+    }
+    send404(){
+        this.res.writeHead(404)
+        this.res.end()
+    }
+    send500(){
+        this.res.writeHead(500)
+        this.res.end()
+    }
+
+}
